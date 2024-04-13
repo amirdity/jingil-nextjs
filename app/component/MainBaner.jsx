6aaa49e8ddac2073d77b26886886585e8a275704
haj amir <amirfattahi22@gmail.com>
2024-04-13 13:26:34 +0330

    starterrr

--- app/component/MainBaner.jsx
+++ app/component/MainBaner.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import Image from "next/image";
 import muge from "../../public/white-mugs.webp";
+import "../component/mainBaner.css";
 function MainBaner() {
   return (
-    <div className="bg-[#6205f7] h-[1000px] w-full flex flex-col lg:flex items-center justify-center text-center ">
-      <div className="">
-        <p className="text-2xl text-white">ماگ</p>
-        <p className="text-2xl text-white">جینگیل پینگیل</p>
-        <p className="text-2xl text-white"> این فرصت رو از دست نده</p>
-        <button className="border-solid border-2 border-white text-2xl text-white">
-          مشاهده محصولات
-        </button>
-      </div>
+    <div className=" h-[800px] w-full flex  items-center justify-center text-center gap-x-10">
       <div className="flex">
         <Image src={muge} />
+      </div>
+      <div className="">
+        <p className="text-3xl text-white">ماگ</p>
+        <p className="text-3xl text-white">جینگیل پینگیل</p>
+        <p className="text-3xl text-white"> این فرصت رو از دست نده</p>
+        <button className="border-solid border-2 border-white text-2xl text-white hover:bg-[#0e2282] rounded-lg">
+          ⬅ مشاهده محصولات
+        </button>
       </div>
     </div>
   );
