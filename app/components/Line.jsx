89d9764135c95bc5a0c4f184343977f03b2d9cb4
amir fattahi <amirfattahi22@gmail.com>
2024-04-16 15:21:28 +0330

    Add files via upload

--- app/components/Line.jsx
+++ app/components/Line.jsx
@@ -1,5 +1,20 @@
 "use client";
-import React from "react";
+
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, History } from 'swiper/modules';
+
+import 'swiper/css/navigation';
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import "./slide1.css";
+
+// import required modules
+
 import { motion } from "framer-motion";
 import Image from "next/image";
 import one from "../../public/1.jpg";
@@ -11,70 +26,58 @@ import six from "../../public/6.jpg";
 import seven from "../../public/7.jpg";
 function Line() {
   return (
-    <div className="flex gap-x-5 justify-end mt-10 mb-10 gap-4 flex-row-reverse font-fa">
-      <motion.div
-        className="  text-center rounded-xl"
-        whileHover={{ y: -7, scale: 1.2 }}
-        whileTap={{ scale: 1 }}
+    <div className=" font-fa h-[40vh] my-10 px-10">
+      <Swiper
+      style={{
+        '--swiper-navigation-color': '#666'
+        
+      }}
+        slidesPerView={3}
+        spaceBetween={10}
+        navigation={true}
+        
+        history={{
+          key: 'slide',
+        }}
+        
+        breakpoints={{
+          200: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+          750: {
+            slidesPerView: 5,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 7,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Navigation, History]}
+        className="mySwiper "
       >
-        <Image src={one} />
-        <p>ماگ</p>
-        <p> ⬅ نمایش </p>
-      </motion.div>
-      <motion.div
-        className="  text-center"
-        whileHover={{ y: -7, scale: 1.2 }}
-        whileTap={{ scale: 1 }}
-      >
-        <Image src={two} />
-        <p>ماگ</p>
-        <p> ⬅ نمایش </p>
-      </motion.div>{" "}
-      <motion.div
-        className="  text-center"
-        whileHover={{ y: -7, scale: 1.2 }}
-        whileTap={{ scale: 1 }}
-      >
-        <Image src={three} />
-        <p>ماگ</p>
-        <p> ⬅ نمایش </p>
-      </motion.div>{" "}
-      <motion.div
-        className="  text-center"
-        whileHover={{ y: -7, scale: 1.2 }}
-        whileTap={{ scale: 1 }}
-      >
-        <Image src={fore} />
-        <p>ماگ</p>
-        <p> ⬅ نمایش </p>
-      </motion.div>{" "}
-      <motion.div
-        className="  text-center"
-        whileHover={{ y: -7, scale: 1.2 }}
-        whileTap={{ scale: 1 }}
-      >
-        <Image src={five} />
-        <p>ماگ</p>
-        <p> ⬅ نمایش </p>
-      </motion.div>{" "}
-      <motion.div
-        className="  text-center"
-        whileHover={{ y: -7, scale: 1.2 }}
-        whileTap={{ scale: 1 }}
-      >
-        <Image src={six} />
-        <p>ماگ</p>
-        <p> ⬅ نمایش </p>
-      </motion.div>{" "}
-      <motion.div
-        className="  text-center"
-        whileHover={{ y: -7, scale: 1.2 }}
-        whileTap={{ scale: 1.5 }}
-      >
-        <Image src={seven} />
-        <p>ماگ</p>
-        <p> ⬅ نمایش </p>
-      </motion.div>
+        <SwiperSlide>
+          <div className="flex flex-col ">
+            <motion.div
+              whileHover={{ y: -25, scale: 1.2 }}
+              whileTap={{ scale: 1 }} 
+              className="mb-10 rounded-3xl"
+            >
+              <Image src={seven} className="rounded-2xl" />
+            </motion.div>
+
+            <p className="hover:text-[#f59931] "> ماگ استیل</p>
+            <p className="hover:text-[#f59931] "> نمایش ⬅</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+      </Swiper>
     </div>
   );
 }
