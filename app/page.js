2db0aaabf6052df22842c849f333320ad69dad8e
amir fattahi <amirfattahi22@gmail.com>
2024-04-18 09:47:38 +0330

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -6,7 +6,13 @@ import Slide from "./components/Line/Line1";
 import SlideT from "./components/Line/Line2";
 import GridBox from "./components/GridBox";
 import FooterOfHeader from "./components/Header/FooterOfHeader";
+// LOADING PAGE
+// import { useState, CSSProperties } from "react";
+// import ClipLoader from "react-spinners/ClipLoader";
+
+
 export default function Home() {
+ 
   return (
     <main>
       <Header />
